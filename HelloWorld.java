e1428f2c0121c4417f2031844a7790224cc34be4
Rahul30121993 <61008134+Rahul30121993@users.noreply.github.com>
2020-02-19 11:42:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Welcome to CI class");
+      System.out.println("Hello all, Welcome to CI class on 19/2/2020");
    }
 }
